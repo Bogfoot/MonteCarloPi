63a72c2a08e9b31d0951820ca46d89b411964890
Oleg Vojvodić <oleg.vojvodic@outlook.com>
2020-06-10 22:04:47 +0200

    Popravljen ROOT prozor. Reagira na user input.

--- MonteCarloPi.h
+++ MonteCarloPi.h
@@ -5,6 +5,54 @@
 #include <string>
 #include <chrono>
 
+#include <TApplication.h>
+#include <TF1.h>
+#include <TH1F.h>
+#include <TCanvas.h>
+#include <TFrame.h>
+#include <TPaveLabel.h>
+#include <TPaveText.h>
+#include <TArrow.h>
+#include <TSystem.h>
+#include <TGraphErrors.h>
+
+
+// Klasa MonteCarloApp sluzi za grupiranje UI elementa i omogucava multi-thread rad.
+class MonteCarloApp : public TApplication
+{
+public:
+
+	MonteCarloApp() : TApplication{ "ROOT Application", nullptr, nullptr }
+	{
+	}
+
+	// Ovdje treba dodati sve UI elemente vidljive na TCanvas-u.
+	TCanvas canvas{ "c1" };
+	TH1F th1f{ "th1f", "", 11, 2.0, 4.0 };
+
+	virtual void StartIdleing()
+	{
+		TApplication::StartIdleing();
+
+		// Re-draw when data changes.
+		if (invalidated)
+		{
+			invalidated = false;
+
+			th1f.Draw();
+			canvas.Update();
+		}
+	}
+
+	void Invalidate()
+	{
+		invalidated = true;
+	}
+
+private:
+	bool invalidated = false;
+};
+
 
 constexpr int MaxIterations = 11;
 using PiArray = std::array<double, MaxIterations>;
@@ -18,7 +66,7 @@ void PrintPiMatrix(const PiMatrix& pis, int size);
 int UiPromptInteger(const std::string& prompt, int min, int max);
 char UiPromptChar(const std::string& prompt);
 void ConsolePrintResults(const PiMatrix& pis, const PiArray& avg, const PiArray& stDev, int n);
-void GuiPrintResults(const PiMatrix& pis, const PiArray& avg, const PiArray& stDev, int n);
+void GuiPrintResults(MonteCarloApp* app, const PiMatrix& pis, const PiArray& avg, const PiArray& stDev, int n);
 void Potencija(int n);
 
 
@@ -29,7 +77,7 @@ struct Stopwatch
       start = std::chrono::high_resolution_clock::now();
    }
 
-   double StopAndElapsedMilliseconds()
+   double ElapsedMilliseconds()
    {
       using namespace std::chrono;
 
